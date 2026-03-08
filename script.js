588dfe5f236b0f9d2a53dadc2517adc6eb6c2870
qw9852367411 <qw9852367411@outlook.com>
2026-03-08 09:48:23 +0800

    BUG修复

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // 数字动画效果
 function animateNumbers() {
-    const numberElements = document.querySelectorAll('.stat-number:not(.animated):not(#server-run-time)');
+    const numberElements = document.querySelectorAll('.stat-number:not(.animated):not(#server-run-time):not(#server-status)';
     
     numberElements.forEach(element => {
         // 添加标记，防止重复动画
